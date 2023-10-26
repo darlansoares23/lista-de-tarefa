d52b65b05b84da2617279aaeefdf2d3f8b12c32f
darleigo <soaresdarlan520@gmail.com>
2023-10-26 16:47:20 -0300

    início das tarefas

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>teste bacana</h1>
+      </div>
+      <input type="button" value="adicionar" />
+    </>
+  )
+}
+
+export default App
