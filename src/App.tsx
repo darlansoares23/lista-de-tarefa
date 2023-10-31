c323cf31545f47161d67433b8fdb049eb29f81e4
darlansoares23 <soaresdarlan520@gmail.com>
2023-10-31 01:50:53 -0300

    eu tentei fazer alguma coisa e agora eu só quero apagar tudo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import { useRef, useState } from 'react';
 import './App.css'
 
+type TTarefa = {
+  id: number;
+  tarefa: string;
+  pronto: boolean;
+};
+
 function App() {
-  return (
+  const [tarefas, setTarefas] = useState<TTarefa[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const criarTarefa = () => {
+    if (inputRef.current)
+    setTarefas{(id: Math.random(), tarefa: inputRef.current.value, pronto: false},...tarefas,)
+  }
     <>
+    {tarefas.map((tarefa)=><div key=(tarefa.id)}>
+    <span>(tarefa.tarefa)</span>
+    <button>Pronto</button>  
+    </div>
+
       <div id='coiso'>
         <h1>teste bacana</h1>
-        <input id='butão' type= "text" placeholder='digite sua tarefa' required />
+        <form onSubmit={(e)=> {e.preventDefault();
+        criarTarefa();
+        }}> 
+        <input ref={inputRef} maxLength={30} id='botão' type= "text" placeholder='digite sua tarefa' required />
+        </form>
       <button>adicionar</button>
       </div>
     </>
